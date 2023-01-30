4d1c2c798b9b24e4ce086a2aab2f0208cacf21ad
Yousef Shaban <yousefshaban.eg@gmail.com>
2023-01-30 07:26:07 +0200

    .

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,10 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
-  console.log('Tour: ', tour);
-  console.log('User: ', user);
-  console.log('Price: ', price);
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
